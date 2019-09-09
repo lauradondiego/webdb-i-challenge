149f81deb5323f987d1f1ba7da4581a09619699c
laura <lauradondiego@gmail.com>
2019-09-09 18:00:21 -0400

    completed stretch goals for queries.md

--- data/accounts/account-router.js
+++ data/accounts/account-router.js
@@ -4,7 +4,7 @@ const db = require("../dbConfig");
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  db("accounts")
+  db("accounts") // same as saying (db.select('*').from('accounts'))
     .select("id", "name", "budget")
     .then(accounts => {
       res.status(200).json(accounts);
@@ -15,7 +15,27 @@ router.get("/", (req, res) => {
 });
 // working and returning all accounts in database
 
-router.post("/", (req, res) => {
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("accounts")
+    .where({ id })
+    .then(accounts => {
+      // we must check the length to find our if our user exists
+      if (accounts.length) {
+        res.json(accounts);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find account with given id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get accoount" });
+    });
+});
+// getting account by ID
+
+router.post("/", validateAccountPost, (req, res) => {
   const postAccount = req.body;
   db("accounts")
     .insert(postAccount, "id")
@@ -44,6 +64,7 @@ router.put("/:id", (req, res) => {
       res.json(err);
     });
 });
+// working and updating by ID - returns a number of accounts changed
 
 router.delete("/:id", (req, res) => {
   db("accounts")
@@ -56,5 +77,17 @@ router.delete("/:id", (req, res) => {
       res.json(err);
     });
 });
+// working and deleting by ID - returns a number of accounts deleted
 
+// Custom Middleware
+function validateAccountPost(req, res, next) {
+  if (Object.keys(req.body) < 1) {
+    res.status(400).json({ message: "missing account data" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else if (!req.body.budget) {
+    res.status(400).json({ message: "missing required budget field" });
+  }
+  next();
+}
 module.exports = router;
